bb201e90758124c165af70c2779b189062ebc6f5
Mrowr Purr <mrowr.purr@gmail.com>
2022-02-27 00:54:09 -0800

    Callback lambda OK

--- src/VRKeyboardPapyrus.h
+++ src/VRKeyboardPapyrus.h
@@ -19,30 +19,31 @@ namespace VRKeyboard {
         void GetKeyboardInput(RE::BSScript::IVirtualMachine*, RE::VMStackID stackId, StaticFunctionTag*, std::string_view callbackFn) {
             logger::info("Invoked GetKeyboardInput()");
 
-            // auto* vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
+            auto* vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
 
-            // RE::BSSpinLockGuard lock(vm->runningStacksLock);
+            RE::BSSpinLockGuard lock(vm->runningStacksLock);
 
-            // const auto matchingStacks = vm->allRunningStacks.find(stackId);
-            // if (matchingStacks == vm->allRunningStacks.end()) return;
-            // const auto& stack = matchingStacks->second;
-            // auto variable = stack->top->previousFrame->self;
+            const auto matchingStacks = vm->allRunningStacks.find(stackId);
+            if (matchingStacks == vm->allRunningStacks.end()) return;
+            const auto& stack = matchingStacks->second;
+            auto variable = stack->top->previousFrame->self;
 
-            // #undef GetObject
-            // const auto& object = variable.GetObject();
-            // const auto className = object->GetTypeInfo()->GetName();
-            // const auto handle = object->GetHandle();
+            #undef GetObject
+            const auto& object = variable.GetObject();
+            const auto className = object->GetTypeInfo()->GetName();
+            const auto handle = object->GetHandle();
 
-            // auto args = RE::FunctionArguments<std::string>("Hello this is the keyboard text");
-            // auto callback = new MyCallback();
-            // auto callbackPtr = RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>(callback);
+            auto callback = new MyCallback();
+            auto callbackPtr = RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>(callback);
 
-            VRKeyboard::Open([] (std::string text) {
+            VRKeyboard::Open([vm, handle, className, callbackFn, &callbackPtr] (std::string text) {
                 logger::info("Hi from inside of lambda");
                 logger::info("The text is: '{}'", text);
                 const auto& consoleLog = RE::ConsoleLog::GetSingleton();
                 consoleLog->Print(text.c_str());
                 consoleLog->Print(std::format("Got text! '{}'", text).c_str());
+                auto args = RE::FunctionArguments<std::string>(std::basic_string(text));
+                vm->DispatchMethodCall(handle, className, callbackFn, &args, callbackPtr);
             });
 
             // vm->DispatchMethodCall(handle, className, callbackFn, &args, callbackPtr);
