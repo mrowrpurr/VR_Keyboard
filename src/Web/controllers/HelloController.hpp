5605c5990348f23c5a34b9e0b783a9fbf73c4fa7
Mrowr Purr <mrowr.purr@gmail.com>
2022-02-26 17:51:12 -0800

    Got keyboard input and printed it!

--- src/Web/controllers/HelloController.hpp
+++ src/Web/controllers/HelloController.hpp
@@ -2,10 +2,13 @@
 
 #include <iostream>
 #include <format>
+#include <thread>
+#include <windows.h>
 #include <openvr.h>
 #include <oatpp/web/server/api/ApiController.hpp>
 #include <oatpp/core/macro/codegen.hpp>
 #include <oatpp/core/macro/component.hpp>
+#include <RE/C/ConsoleLog.h>
 
 #include "Web/dtos/TestDto.hpp"
 
@@ -15,30 +18,45 @@ class HelloController : public oatpp::web::server::api::ApiController {
 public:
   HelloController(OATPP_COMPONENT(std::shared_ptr<ObjectMapper>, objectMapper)) : oatpp::web::server::api::ApiController(objectMapper) {}
 
-public:
-  
+  vr::VROverlayHandle_t keyboardOverlayHandle = 0;
+  std::thread* keyboardHandlerThread = nullptr;
+  vr::COpenVRContext vrContext = vr::COpenVRContext();
+
+  void ListenToVrKeyboard() {
+    vr::EVROverlayError err;
+    err = vrContext.VROverlay()->ShowKeyboardForOverlay(keyboardOverlayHandle, vr::k_EGamepadTextInputModeNormal, vr::k_EGamepadTextInputLineModeSingleLine, 0, "The Description", 0, "Existing text", 0);
+    if (err != vr::EVROverlayError::VROverlayError_None) {
+      return;
+    }
+    Sleep(100);
+
+    vr::VREvent_t event;
+
+    while (true) {
+      if (vrContext.VROverlay()->PollNextOverlayEvent(keyboardOverlayHandle, &event, sizeof(vr::VREvent_t))) {
+        if (event.eventType == vr::VREvent_KeyboardClosed || event.eventType == vr::VREvent_KeyboardDone) {
+          char buffer[128 + 1] = "\0";
+          vrContext.VROverlay()->GetKeyboardText(buffer, 128);
+          auto text = std::string(buffer);
+          auto output = std::format("Keyboard text entered: '{}'", text);
+          auto* consoleLog = RE::ConsoleLog::GetSingleton();
+          consoleLog->Print(output.c_str());
+        }
+      }
+      Sleep(50);
+    }
+  }
+
   ENDPOINT("GET", "/", root) {
     auto now = std::chrono::system_clock::now();
-
-    auto vrContext = vr::COpenVRContext();
-
-    vr::VROverlayHandle_t keyboardOverlayHandle = 0;
-//     std::thread* keyboardHandlerThread = nullptr;
-
+    
     vr::EVROverlayError err;
     err = vrContext.VROverlay()->CreateOverlay("The Key", "The Title", &keyboardOverlayHandle);
     if (err != vr::EVROverlayError::VROverlayError_None) {
       return response("Could not create VR overlay!");
     }
 
-    auto description = "Enter text please!";
-    auto existingText = "Foo";
-
-    vr::EVROverlayError err2;
-    err2 = vrContext.VROverlay()->ShowKeyboardForOverlay(keyboardOverlayHandle, vr::k_EGamepadTextInputModeNormal, vr::k_EGamepadTextInputLineModeSingleLine, 0, "The Description", 0, "Existing text", 0);
-    if (err != vr::EVROverlayError::VROverlayError_None) {
-      return response("Could not show keyboard");
-    }
+    keyboardHandlerThread = new std::thread([this] { this->ListenToVrKeyboard(); });
 
     #ifdef FOO
       return response(std::format("FOO! Hi Skyrim, I am C++. I will run in VR shortly! The time is currently {}", now));
